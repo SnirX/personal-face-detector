291335a5c28ac3140ec22092b8616dac91253e20
SnirX <snir994@gmail.com>
2020-06-25 01:35:34 +0300

    PGD

--- App/FoolMe/pgd.py
+++ App/FoolMe/pgd.py
@@ -12,36 +12,31 @@ from App.FoolMe.exceptions.fool_me_exceptions import NoLabelException
 embedding_wrapper = EmbeddingWrapper()
 
 
-def run_pgd(source_tensor, target_label, epsilon=0.045, epochs=2):
+def run_pgd(source_tensor, target_label, epsilon=0.045, epochs=3):
     if not embedding_wrapper.is_label_exists(target_label):
         raise NoLabelException()
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     resnet = InceptionResnetV1(pretrained='vggface2').eval().to(device).train(False)
 
-    random_imgs_with_label = [(transforms.ToPILImage()(source_tensor.squeeze(0)).convert("RGB"), 'shabi')]
-
     targets_dict = defaultdict(
         lambda: {"average_vector": torch.FloatTensor([[0] * 512]).to(device), "amount_of_vectors": 0})
 
     targets_dict_tensors = embedding_wrapper.get_embeddings_by_label(target_label)
-    targets_dict[target_label]['average_vector'] = embedding_wrapper.get_mean_embedding_of_embedding_set(
-        targets_dict_tensors)
+    targets_dict[target_label]['average_vector'] = embedding_wrapper.get_mean_embedding_of_embedding_set(targets_dict_tensors)
     targets_dict[target_label]['amount_of_vectors'] = len(targets_dict_tensors)
 
-    random_tensors = []
-    for image, person_in_image in random_imgs_with_label:
-        tensor = transforms.ToTensor()(image)
-        stacked_tensor = torch.stack([tensor]).to(device)  # Tensor([1,2,3]) -> Tensor([[1,2,3]])
-        random_tensors.append(stacked_tensor)
+    image = transforms.ToPILImage()(source_tensor.squeeze(0)).convert("RGB")
+    tensor = transforms.ToTensor()(image)
+    stacked_tensor = torch.stack([tensor]).to(device)  # Tensor([1,2,3]) -> Tensor([[1,2,3]])
 
     target_embedded_vector = targets_dict.get(target_label).get('average_vector').unsqueeze(0)
 
     start_time = time.time()
     is_first = True
+    image_with_noise = stacked_tensor
     for epoch in range(epochs):
         print("target : {} , epsilon : {}, epoch : {}".format(target_label, epsilon, epoch + 1))
-        for tensor in random_tensors:
-            image_with_noise = TFGSM(tensor, resnet, target_embedded_vector, epsilon, requires_grad=is_first)
+        image_with_noise = TFGSM(image_with_noise, resnet, target_embedded_vector, epsilon, requires_grad=is_first)
         is_first = False
     score = embedding_wrapper.get_distance_between_embeddings(target_embedded_vector, resnet(image_with_noise))
     print("Time took for pgd on target {} : {} seconds".format(target_label, time.time() - start_time))
